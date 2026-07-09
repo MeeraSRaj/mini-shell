2906ece8d5b7f7b1de69ff5045d48836daed6821
MeeraSRaj <mithunsrajrajesh@gmail.com>
2026-07-09 22:33:54 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import sys
+import shutil
+import os
 
+BUILT_IN=["exit", "echo", "type"]
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
         command = input()
@@ -11,12 +13,10 @@ def main():
         elif command.startswith("echo "):
             print(command[5:])
         elif command.startswith("type "):
-            if command[5:] == "echo":
-                print("echo is a shell builtin")
-            elif command[5:] == "exit":
-                print("exit is a shell builtin")
-            elif command[5:] == "type":
-                print("type is a shell builtin")
+            if command[5:] in BUILT_IN:
+                print(f"{command[5:]} is a built-in command")
+            elif path := shutil.which(command[5:]):
+                print(f"{command[5:]} is {path}")
             else:
                 print(f"{command[5:]}: not found")
         else:

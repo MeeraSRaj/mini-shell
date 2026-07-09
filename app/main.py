da7c223095235585e9813aadec4fd8ef32378cb3
MeeraSRaj <mithunsrajrajesh@gmail.com>
2026-07-09 17:42:56 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,9 +6,9 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
-        if command == "exit":
+        if command.startswith("echo "):
+            print(command[5:])
             break
-        print(f"{command}: command not found")
         pass
 
 

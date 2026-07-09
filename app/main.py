bffd0b86ad6f1e44269f4023df49a47e767682e2
MeeraSRaj <mithunsrajrajesh@gmail.com>
2026-07-09 22:34:39 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def main():
             print(command[5:])
         elif command.startswith("type "):
             if command[5:] in BUILT_IN:
-                print(f"{command[5:]} is a built-in command")
+                print(f"{command[5:]} is a shell built-in")
             elif path := shutil.which(command[5:]):
                 print(f"{command[5:]} is {path}")
             else:

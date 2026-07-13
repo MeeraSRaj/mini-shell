c846b6422c418e99afc3f972288526fefdf7bdb4
MeeraSRaj <mithunsrajrajesh@gmail.com>
2026-07-13 23:01:33 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ BUILT_IN=["exit", "echo", "type", "pwd", "cd"]
 def main():
     while True:
         redirect_file=None
+        append_mode=False
         error_file=None
         sys.stdout.write("$ ")
         command = input()
@@ -24,21 +25,29 @@ def main():
                 idx=parts.index("1>")
             redirect_file=parts[idx+1]
             parts=parts[:idx]
+        if ">>" in parts or "1>>" in parts:
+            append_mode=True
+            if ">>" in parts:
+                idx=parts.index(">")
+            elif "1>>" in parts:
+                idx=parts.index("1>")
+            redirect_file=parts[idx+1]
+            parts=parts[:idx]
         if "2>" in parts:
             idx=parts.index("2>")
             error_file=parts[idx+1]
             parts=parts[:idx]
 
-
+        mode="a" if append_mode else "w"
         if parts[0] == "exit":
             break
         elif parts[0]=="echo":
             output=" ".join(parts[1:])
             if redirect_file:
-                with open(redirect_file,"w") as f:
+                with open(redirect_file,mode) as f:
                     f.write(output+"\n")
             elif error_file:
-                open(error_file, "w").close()
+                open(error_file, mode).close()
                 print(output)
             else:
                 print(output)
@@ -47,21 +56,21 @@ def main():
             if parts[1] in BUILT_IN:
                 output=f"{parts[1]} is a shell builtin"
                 if redirect_file:
-                    with open(redirect_file,"w") as f:
+                    with open(redirect_file,mode) as f:
                         f.write(output+"\n")
                 else:
                     print(output)
             elif path := shutil.which(parts[1]):
                 output=f"{parts[1]} is {path}"
                 if redirect_file:
-                    with open(redirect_file,"w") as f:
+                    with open(redirect_file,mode) as f:
                         f.write(output+"\n")
                 else:
                     print(output)
             else:
                 output=f"{parts[1]}: not found"
                 if error_file:
-                    with open(error_file,"w") as f:
+                    with open(error_file,mode) as f:
                         f.write(output)
                 else:
                     print(output)
@@ -78,7 +87,7 @@ def main():
             except FileNotFoundError:
                 output=f"cd: {dir}: No such file or directory"
                 if error_file:
-                    with open(error_file,"w") as f:
+                    with open(error_file,mode) as f:
                         f.write(output)
                 else:
                     print(output)
@@ -86,10 +95,10 @@ def main():
         elif path := shutil.which(parts[0]):
             exec_path=path
             if redirect_file:
-                with open(redirect_file,"w") as f:
+                with open(redirect_file,mode) as f:
                     subprocess.run(parts, executable=exec_path,stdout=f)
             elif error_file:
-                with open(error_file,"w") as f:
+                with open(error_file,mode) as f:
                     subprocess.run(parts,executable=exec_path,stderr=f)
             else:
                 subprocess.run(parts, executable=exec_path)
